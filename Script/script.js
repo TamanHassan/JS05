fc0820d54dd23f4105a19eee4717f3c0190515d5
Taman Hassan <tamanhassan1@gmail.com>
2025-11-04 16:57:05 +0100

    updared code script.js

--- Script/script.js
+++ Script/script.js
@@ -41,8 +41,54 @@ const account = {
 
     exitAccount: function() {
         console.log("Exiting account.");
+        this.accountName = "";
+        this.balance = 0;
+        console.log("Account data cleared.");
     }
     };
+
+    function atm() {
+        let action;
+        do {
+            action = prompt(
+                `Choose an action:
+                1.View balance
+                2.Deposit Money
+                3.Withdraw Money
+                4.Exit Account
+                5.Change Account Name
+                6.Exit ATM (Log Out) `
+            );
+
+            switch(action) {
+                case '1':
+                    account.getBalance();
+                    break;
+                case '2':
+                    let depositAmount = parseFloat(prompt("Enter amount to deposit:"));
+                    account.deposit(depositAmount);
+                    break;
+                case '3':
+                    let withdrawalAmount = parseFloat(prompt("Enter amount to withdraw:"));
+                    account.withdrawal(withdrawalAmount);
+                    break;
+                case '4':
+                    account.exitAccount();
+                    break;
+                case '5':
+                    let newName = prompt("Enter new account name:");
+                    account.accountName = newName;
+                    console.log(`Account name changed to ${newName}.`);
+                    break;
+                case '6':
+                    console.log("Logging out of ATM.");
+                    break;
+                default:
+                    console.log("Invalid action. Please try again.");
+            }
+        } while (action !== '6');
+    }
+
 
     account.getAccountName(); 
     account.getBalance(); 
